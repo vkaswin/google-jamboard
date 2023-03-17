66ee75762488ee22b1f6bcf1c5907ba1ffaaa7b1
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-03-17 12:58:17 +0530

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className="App">
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn
       </p>
     </div>
   );
