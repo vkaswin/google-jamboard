fe35b8f6638f31fcdce87257d8df4c97ed7c73c5
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-03-17 15:49:45 +0530

    update

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -5,10 +5,48 @@ let Document = lazy(
   () => import(/* webpackChunkName: "[Document]" */ "@/pages/Document")
 );
 
+let AuthLayout = lazy(
+  () => import(/* webpackChunkName: "[AuthLayout]" */ `@/layouts/AuthLayout`)
+);
+
+let SignIn = lazy(
+  () => import(/* webpackChunkName: "[SignIn]" */ "@/pages/SignIn")
+);
+
+let SignUp = lazy(
+  () => import(/* webpackChunkName: "[SignUp]" */ "@/pages/SignUp")
+);
+
 let routes: RouteObject[] = [
   {
     path: "/",
     element: <Navigate to="/document/64106505659e51ce8d788753" replace />,
+  },
+  {
+    path: "/auth",
+    element: (
+      <Suspense fallback="Loading...">
+        <AuthLayout />
+      </Suspense>
+    ),
+    children: [
+      {
+        path: "sign-in",
+        element: (
+          <Suspense fallback="Loading...">
+            <SignIn />
+          </Suspense>
+        ),
+      },
+      {
+        path: "sign-up",
+        element: (
+          <Suspense fallback="Loading...">
+            <SignUp />
+          </Suspense>
+        ),
+      },
+    ],
   },
   {
     path: "/document/:id",
