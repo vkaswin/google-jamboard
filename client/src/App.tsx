e2813759f2fdf3c804478a21f36457f13089ec47
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-03-17 11:57:36 +0530

    update

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "@/assets/scss/index.scss";
+
+const App = () => {
+  return (
+    <div className="App">
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/main.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+let container = document.getElementById("root") as HTMLElement;
+
+let root = createRoot(container);
+
+root.render(<App />);
