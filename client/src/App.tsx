1e88d044a29a0a4b5c9bfd297c40b7581dd5e751
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-03-20 11:12:00 +0530

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
-import { Fragment, Suspense } from "react";
+import { Fragment, Suspense, useEffect } from "react";
 import { HashRouter } from "react-router-dom";
 import AuthProvider from "@/context/AuthContext";
 import Router from "@/router";
+import useAuth from "./hooks/useAuth";
 
 import "@/assets/scss/index.scss";
 
 const App = () => {
+  let { logout } = useAuth();
+
+  useEffect(() => {
+    document.addEventListener("unauthorized", logout);
+    return () => {
+      document.removeEventListener("unauthorized", logout);
+    };
+  }, []);
+
   return (
     <Fragment>
       <HashRouter>
